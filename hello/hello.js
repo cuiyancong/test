07a2b1307ad614ba1a7bd994c4baf75a54ff9358
cyc <2438165162@qq.com>
2017-11-15 10:56:30 +0800

    text

---
+++ hello/hello.js
@@ -0,0 +1,8 @@
+const http = require('http')
+http.createServer(function(req,res) {
+res.writeHead(200,{'Content-Type':'text/plain'})
+res.end('hello world')
+}).listen(8081)
+
+console.log('server test')
+
